0e7082fa8cf7051bb63716812da702ed74a47e97
rashmi-22-04 <85274170+rashmi-22-04@users.noreply.github.com>
2023-10-24 12:13:59 +0530

    Implement API

--- src/Component/weatherApp/WeatherApp.jsx
+++ src/Component/weatherApp/WeatherApp.jsx
@@ -13,11 +13,24 @@ const WeatherApp = () => {
 
   let api_key="ebcf4ae45b2ecb126af1812367782091";
   
-  const serach=()=>{
+  const serach= async ()=>{
    const element=document.getElementsByClassName("cityInput");
    if(element[0].value===""){
     return 0;
    }
+
+   let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+   let response =await fetch(url);
+   let data= await response.json();
+   const humidity=document.getElementsByClassName("humidity-percentage");
+   const wind=document.getElementsByClassName("wind-rate");
+   const temperature=document.getElementsByClassName("weather-temp");
+   const location=document.getElementsByClassName("weather-location");
+
+   humidity[0].innerHTML=data.main.humidity+" %";
+   wind[0].innerHTML=data.wind.speed+" km/hr";
+   temperature[0].innerHTML=data.main.temp+" °C";
+   location[0].innerHTML=data.name;
   }
   
 
@@ -32,20 +45,20 @@ const WeatherApp = () => {
       <div className="weather-img">
          <img src={cloud_icon} alt=''/>
       </div>
-      <div className="weather-temp">24°C</div>
-      <div className="weather-location">London</div>
+      <div className="weather-temp"></div>
+      <div className="weather-location"></div>
       <div className='data-container'>
         <div className="element">
           <img src={humidity_icon} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percentage">64%</div>
+            <div className="humidity-percentage"></div>
             <div className="text">Humidity</div>
           </div>
         </div>
         <div className="element">
           <img src={wind_icon} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percentage">18 km/hr</div>
+            <div className="wind-rate"></div>
             <div className="text">Wind Speed</div>
           </div>
         </div>
